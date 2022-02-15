61c6e61db55060ca318ded13e4a27b5a76339863
iammmai <duongm279@gmail.com>
2022-02-15 15:56:40 +0100

    use bodyparse

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import Koa from "koa";
 import mongoose from "mongoose";
 import bodyParser from "koa-bodyparser";
+import cors from "@koa/cors";
 import "dotenv/config";
 import router from "./router.js";
 
@@ -13,8 +14,10 @@ mongoose.connect(process.env.MONGO_URI, {
 mongoose.connection.on("error", console.error);
 
 const app = new Koa();
-app.use(bodyParser());
-
-app.use(router.routes()).use(router.allowedMethods());
+app
+  .use(cors({ credentials: true }))
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(8080);
