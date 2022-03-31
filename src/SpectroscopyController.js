8c05e6e984c4af0ca925240565540b99fb9efd75
iammmai <duongm279@gmail.com>
2022-03-31 14:41:54 +0200

    create fomulas on spec create

--- src/SpectroscopyController.js
+++ src/SpectroscopyController.js
@@ -1,26 +1,48 @@
 import * as R from "ramda";
 import SpectroscopyModel from "./SpectroscopySchema.js";
+import FormulaModel from "./FormulaSchema.js";
 
 const SpectroscopyController = {
   get: async (ctx) => {
-    const _id = R.path(["params", "id"], ctx);
-    ctx.body = await SpectroscopyModel.findOne({ _id });
+    try {
+      const _id = R.path(["params", "id"], ctx);
+      ctx.body = await SpectroscopyModel.findOne({ _id });
+    } catch (error) {
+      ctx.body = error;
+    }
   },
   getAll: async (ctx) => {
     ctx.body = await SpectroscopyModel.find({});
   },
   create: async (ctx) => {
-    const spec = new SpectroscopyModel(R.path(["request", "body"], ctx));
-    await spec.save().catch((err) => (ctx.body = err));
-    ctx.body = spec;
+    try {
+      const spec = new SpectroscopyModel(
+        R.path(["request", "body", "spectroscopy"], ctx)
+      );
+      await spec.save().catch((err) => (ctx.body = err));
+      R.path(["request", "body", "processes"], ctx).forEach(async (process) => {
+        const formula = new FormulaModel({
+          ...process,
+          spectroscopyId: spec._id,
+        });
+        await formula.save().catch((err) => (ctx.body = err));
+      });
+      ctx.body = spec;
+    } catch (error) {
+      ctx.body = error;
+    }
   },
   update: async (ctx) => {
-    const _id = R.path(["params", "id"], ctx);
-    ctx.body = await SpectroscopyModel.findOneAndUpdate(
-      { _id },
-      R.path(["request", "body"], ctx),
-      { new: true }
-    );
+    try {
+      const _id = R.path(["params", "id"], ctx);
+      ctx.body = await SpectroscopyModel.findOneAndUpdate(
+        { _id },
+        R.path(["request", "body"], ctx),
+        { new: true }
+      );
+    } catch (error) {
+      ctx.body = error;
+    }
   },
 };
 
